cc3287d21ef726ef508ad58bd1f6c183e1c29da5
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-02-17 01:05:25 +0500

    Customize stripe form

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,6 @@
 class ChargesController < ApplicationController
   def create
     # Amount in cents
-    @amount = 500
   
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
@@ -10,7 +9,7 @@ class ChargesController < ApplicationController
   
     charge = Stripe::Charge.create({
       customer: customer.id,
-      amount: @amount,
+      amount: params[:amount],
       description: 'Rails Stripe customer',
       currency: 'usd',
     })
