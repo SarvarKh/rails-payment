6dd292681a61b44b9d11587c0f38debd5455490a
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-02-17 01:25:00 +0500

    Show what have been purchased

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,21 +1,28 @@
 class ChargesController < ApplicationController
-  def create
-    # Amount in cents
-  
-    customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-    })
-  
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: params[:amount],
-      description: 'Rails Stripe customer',
-      currency: 'usd',
-    })
-  
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-  end
+	
+	def create
+	  # Amount in cents
+
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :source  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => params[:amount],
+	    :description => 'Rails Stripe customer',
+	    :currency    => 'usd'
+	  )
+
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+	  	amount: params[:amount], description: charge.description, currency: charge.currency,
+	  	customer_id: customer.id, product_id: 1)
+
+	  redirect_to purchase
+
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to new_charge_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :charges
+  resources :purchases, only: [:show]
 end
